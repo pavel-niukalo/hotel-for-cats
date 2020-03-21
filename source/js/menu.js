94636c6d6e7d52ebcb2f5589e02df868d356d243
Pavel Nyukalo <PashaOktan07@gmail.com>
2020-03-21 21:48:18 +0500

    Завершает меню и почти промо-блок

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,8 +1,38 @@
 'use strict';
 
 (function () {
-  var mainNav = document.querySelector('.main-nav');
+  var pageHeader = document.querySelector('.page-header');
+  var mainNav = pageHeader.querySelector('.main-nav');
+  var mainNavOpen = mainNav.querySelector('.main-nav__open');
+  var mainNavClose = mainNav.querySelector('.main-nav__close');
+  var mainNavLinks = mainNav.querySelectorAll('.main-nav__link');
 
   mainNav.classList.remove('main-nav--nojs');
   mainNav.classList.add('main-nav--closed');
+
+  var openNav = function() {
+    mainNav.classList.remove('main-nav--closed');
+    mainNav.classList.add('main-nav--opened');
+    pageHeader.classList.add('page-header--opened');
+  };
+
+  var closeNav = function() {
+    mainNav.classList.remove('main-nav--opened');
+    pageHeader.classList.remove('page-header--opened');
+    mainNav.classList.add('main-nav--closed');
+  };
+
+  mainNavOpen.addEventListener('click', function() {
+    openNav();
+  });
+
+  mainNavClose.addEventListener('click', function() {
+    closeNav();
+  });
+
+  mainNavLinks.forEach(function (item) {
+    item.addEventListener('click', function() {
+      closeNav();
+    });
+  });
 })();
