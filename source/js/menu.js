78fc8cc0036614a40c153b22303d0ebfaedcf9f9
Pavel Nyukalo <PashaOktan07@gmail.com>
2020-03-21 18:34:40 +0500

    Начинает оформление меню
    + js к меню

---
+++ source/js/menu.js
@@ -0,0 +1,8 @@
+'use strict';
+
+(function () {
+  var mainNav = document.querySelector('.main-nav');
+
+  mainNav.classList.remove('main-nav--nojs');
+  mainNav.classList.add('main-nav--closed');
+})();
