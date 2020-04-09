f44017ec645ea523653578ab5bc99c19a1d64202
Pavel Nyukalo <PashaOktan07@gmail.com>
2020-04-09 21:41:08 +0500

    Доделывает бронирование

--- js/booking.js
+++ js/booking.js
@@ -16,13 +16,38 @@
     document.querySelector('main')
     .append(booking);
 
-    // var bookingSubmit = document.querySelector('.booking__submit');
+    var onBookingEscPress = function (evt) {
+      window.util.isEscapeEvent(evt, closeBooking);
+    };
 
-    // var closeBooking = function () {
-    //   background.classList.remove('background__page--isActive');
+    var closeBooking = function () {
+      background.classList.remove('background__page--isActive');
 
-    //   booking.remove();
-    // };
+      booking.remove();
+
+      document.removeEventListener('keydown', onBookingEscPress);
+    };
+
+    booking.querySelector('.booking__close')
+    .addEventListener('click', function () {
+      closeBooking();
+    });
+
+    background.addEventListener('click', function () {
+      closeBooking();
+    });
+
+    document.addEventListener('keydown', onBookingEscPress);
+
+    // Отправка формы
+    booking.querySelector('.booking__form')
+    .addEventListener('submit', function (evt) {
+      evt.preventDefault();
+
+      closeBooking();
+
+      window.success.showMessage();
+    })
   };
 
   bookingLinks.forEach(function (item) {
